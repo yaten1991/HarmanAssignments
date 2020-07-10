fd4e8e80b6bab3b7329d4ab53adb6c6f568f9fad
yaten1991 <40053460+yaten1991@users.noreply.github.com>
2020-07-10 17:05:57 +0530

    Update testScript.js

--- HarmanAssignment/js/testScript.js
+++ HarmanAssignment/js/testScript.js
@@ -1,13 +1,14 @@
 // Unit test case to click on button 
 
 setTimeout(() => {
-    unitTest();
-  }, duration);
+  unitTest();
+}, duration);
 
-  async function unitTest() {
-   let els = [...document.getElementsByClassName('btnClass')];
-   for (let j=0; j< els.length; j++){
-     await new Promise(r => setTimeout(r, duration));
-     els[j].click();
-   }
+async function unitTest() {
+  let els = [...document.getElementsByClassName('btnClass')];
+  if (els.length === buttonCount) {
+    tempAlert(`Test case PASS, All ${buttonCount} buttons are available on UI.`);
+  } else {
+    tempAlert(`Test case FAIL, All ${buttonCount} buttons are not available on UI.`);
   }
+}
